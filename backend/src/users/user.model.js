fc1ba32d28bb7b4e5cb4ac1e30af0ec8d8fadb54
Mostafizur <mostafizzfuad@gmail.com>
2025-09-10 23:56:36 +0600

    Create User Model

---
+++ backend/src/users/user.model.js
@@ -0,0 +1,14 @@
+import { Schema, model } from "mongoose";
+
+const userSchema = new Schema({
+    username: { type: String, required: true, unique: true },
+    email: { type: String, required: true, unique: true },
+    password: { type: String, required: true },
+    profileImage: String,
+    bio: { type: String, maxlength: 200 },
+    role: { type: String, enum: ["user", "admin"], default: "user" },
+    createdAt: { type: Date, default: Date.now },
+})
+
+const User = model("User", userSchema);
+module.exports = User;
